7a6e227d7f7e560acff341c8ef33521f43b4e8f3
Paul Brown <paulbrown389@gmail.com>
2024-01-01 22:20:49 -0500

    Mini-Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById('create-task-form').addEventListener('submit', function(event){
+    event.preventDefault();
+    const taskDescription = document.getElementById('new-task-description').value;
+    const task = {
+      id: generateTaskId(),
+      description: taskDescription,
+      priority: 'medium',
+      
+    };
+    taskList.push(task);
+    document.getElementById('new-task-description').value = '';
+    updateTaskList(); 
+  })
 });
+const deleteButton = document.createElement('button');
+deleteButton.innerText = 'Delete';
+deleteButton.addEventListener('click', function() {
+  const taskId = task.id;
+
+  const taskIndex = taskList.findIndex(function(item) {
+    return item.id === taskId;
+  });
+
+  if (taskIndex !== -1) {
+    taskList.splice(taskIndex, 1);
+  }
+});
+
